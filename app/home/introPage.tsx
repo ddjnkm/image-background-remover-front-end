4b399d14541c84ca2bd8ff412c0b3ba8afbb43f5
David Kim <davidkim2552@gmail.com>
2024-10-19 18:26:23 -0400

    fix build issue

--- app/home/introPage.tsx
+++ app/home/introPage.tsx
@@ -7,11 +7,11 @@ const IntroPage: React.FC = () => {
       <div className='w-full h-auto lg:pr-36 md:pr-24 sm:pr-4'>
             <h1 className='text-balance text-3xl pb-5'>Photoshop yourself into cool backgrounds!</h1>
             <div className='lg:flex lg:flex-row gap-2'>
-                <img className='object-scale-down lg:w-96 md:w-64' src={`/home/asset_12.jpg`} alt="image" />
+                <img className='object-scale-down lg:w-96 md:w-64' src="/home/asset_12.jpg" alt="image" />
                 <div className='flex items-center justify-center'>
-                    <p className='text-center'>Put yourself or others into a picture, creating fun and personalized images. 
+                    <p className='text-center'>{`Put yourself or others into a picture, creating fun and personalized images. 
                         Whether it's for a playful edit or a creative project, users can upload photos, select from a variety of backgrounds, 
-                        and easily place yourself or others into a scene all for free!</p>                    
+                        and easily place yourself or others into a scene all for free!`}</p>                    
                 </div>
             </div>
       </div>
--- app/home/uploadImage.tsx
+++ app/home/uploadImage.tsx
@@ -11,7 +11,7 @@ const UploadImage: React.FC = () => {
                 <h1 className='text-3xl pb-5'>Upload an image to get started!</h1>
               </div>
               <div className='flex justify-center text-center'>
-              <p>Upload an image of yourself or someone else and photoshop them into a picture! Super quick, easy and free way to edit someone or something into a background.</p>
+              <p>{`Upload an image of yourself or someone else and photoshop them into a picture! Super quick, easy and free way to edit someone or something into a background.`}</p>
               </div>
               <div className='lg:px-36 md:px-36'>
                 <ImageUpload />
