03d8af2df100fc6b90c038fe76db47cb5c20639d
David Kim <davidkim2552@gmail.com>
2024-10-20 14:10:11 -0400

    update header link

--- app/header.tsx
+++ app/header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
   return (
     <Navbar className="flex justify-between w-full p-5 bg-white">
       <NavbarBrand className="lg:ml-36 md:ml-24">
-        <Link href="/home">
+        <Link href="/">
           <div className="flex flex-row">
             <Image className="sm" src={`home/icon-small.png`} alt="image" />
             <div className="flex items-center justify-center">
