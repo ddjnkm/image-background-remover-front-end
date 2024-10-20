52ada4cb40735a67c43b5168e3314366aaeaba9a
David Kim <davidkim2552@gmail.com>
2024-10-20 14:05:03 -0400

    remove routing from root domain

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
-import { redirect } from 'next/navigation';
+"use client"
+
+import Header from './header';
+import Footnote from "./footer";
+import UploadImage from "./home/uploadImage";
+import IntroPage from './home/introPage';
+import TestImage from './home/testImage';
 
 export default function Home() {
-  redirect('/home')
+  return (
+    <div className="min-h-screen flex flex-col justify-between ">
+        <div>
+        <Header />          
+        </div>
+        <div className="grow 2xl:mx-64 mb-12">
+          <UploadImage />
+          <IntroPage />
+          <TestImage />
+        </div>
+        <div>
+        <Footnote />
+        </div>
+    </div>
+  );
 }
